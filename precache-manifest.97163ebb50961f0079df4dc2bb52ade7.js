3e238bc09edfe7a1eb90c39748dd76ae42bade0d
b-yogesh <yogesh09121995@gmail.com>
2020-09-29 12:48:18 +0200

    Updates

---
+++ precache-manifest.97163ebb50961f0079df4dc2bb52ade7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85054322d112fd416d14dcabc06f5dcd",
+    "url": "/3D-PathFinder/index.html"
+  },
+  {
+    "revision": "5a05a6456c5556fa2503",
+    "url": "/3D-PathFinder/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "26ec7d62afaa06555bdd",
+    "url": "/3D-PathFinder/static/css/main.e6358ef5.chunk.css"
+  },
+  {
+    "revision": "5a05a6456c5556fa2503",
+    "url": "/3D-PathFinder/static/js/2.7a4bc149.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/3D-PathFinder/static/js/2.7a4bc149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26ec7d62afaa06555bdd",
+    "url": "/3D-PathFinder/static/js/main.98e29b2b.chunk.js"
+  },
+  {
+    "revision": "fc3d08134c4fb2393e9d",
+    "url": "/3D-PathFinder/static/js/runtime-main.50836a60.js"
+  },
+  {
+    "revision": "9b41ce9169234e8549311ca3ccedccea",
+    "url": "/3D-PathFinder/static/media/Yeager-Regular.9b41ce91.otf"
+  },
+  {
+    "revision": "e0724aec17b3e93ddeb3f0daefeca552",
+    "url": "/3D-PathFinder/static/media/building1.e0724aec.jpg"
+  },
+  {
+    "revision": "9ce44c23854a4e414aa1fbd5dd39a9f3",
+    "url": "/3D-PathFinder/static/media/building2.9ce44c23.jpg"
+  },
+  {
+    "revision": "74480c90c4eea660f45703864f4bca75",
+    "url": "/3D-PathFinder/static/media/building3.74480c90.jpg"
+  },
+  {
+    "revision": "7547aa786835146407b8a471829d8e12",
+    "url": "/3D-PathFinder/static/media/info.7547aa78.png"
+  },
+  {
+    "revision": "8ae1404e255a2d13f22ffbc1f1db9027",
+    "url": "/3D-PathFinder/static/media/road.8ae1404e.jpg"
+  }
+]);
